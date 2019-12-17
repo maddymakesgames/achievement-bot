c641303a84e44753aba8aa5aab687fe16aaf159a
maddymakesgames <madelinemakesgames@gmail.com>
2019-12-16 22:11:51 -0500

    Removed snekfetch dependency and fixed regex

--- achievementBot.js
+++ achievementBot.js
@@ -1,4 +1,3 @@
-const snek = require('snekfetch');
 const discord = require('discord.js');
 const client = new discord.Client();
 const config = require('./config.json');
@@ -9,7 +8,7 @@ client.on('message', async (msg) => {
     let cmd = msg.content.split('mc!')[1].trim();
     
     if(cmd.startsWith('achievement') || cmd.startsWith('a')) {
-        let args = cmd.match(/\w+|"[^"]+"/g);
+        let args = cmd.match(/[\w.!?\\-]+|"[^"]+"/g);
 		args.shift();
 		msg.channel.send({files:[{attachment:`https://minecraftskinstealer.com/achievement/${encodeURI(`${args[2] ? args[2].replace(/"/g, '') : Math.floor((Math.random()+1)*39)}/${args.length > 1 ? args[0].replace(/"/g, '') : 'Achievement Get!'}/${args.length > 1 ? args[1].replace(/"/g, '') : args[0].replace(/"/g, '')}`)}`, name:'file.png'}]});
     }
